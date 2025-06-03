1259ba4c317de5857a9f959314428a384c87e4fc
athexshu408 <atharvingale408@gmail.com>
2025-06-03 16:49:12 +0530

    new image changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,10 +12,10 @@ export default function Navbar() {
         <div className="wrapper">
             <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}} >Atharv Ingale</motion.span>
              <div className="social">
-                <a href="#"><img src="/facebook.png" alt="" /></a>
-                <a href="#"><img src="/instagram.png" alt="" /></a>
-                <a href="https://linkedin.com/in/atharv-ingale1714"  target="_blank"><img src="/link3.png" alt="" /></a>
-                <a href="https://github.com/atharvInagle0408"  target="_blank"><img src="/github.png" alt="" /></a>
+                <a href="#"><img src="./facebook.png" alt="" /></a>
+                <a href="#"><img src="./instagram.png" alt="" /></a>
+                <a href="https://linkedin.com/in/atharv-ingale1714"  target="_blank"><img src="./link3.png" alt="" /></a>
+                <a href="https://github.com/atharvInagle0408"  target="_blank"><img src="./github.png" alt="" /></a>
              </div>
         </div>
     </div>
